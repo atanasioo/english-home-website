25c5f2621ef64fabe371181ef8efadc5e3551a9e
FatmeHassan <fatimahasan.ishtari@gmail.com>
2022-11-05 11:06:04 +0200

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-3xl font-bold underline bg-slate-400'>
+        hello world!
+      </div>
+    </div>
+  );
+}
+
+export default App;
