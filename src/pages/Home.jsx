1b85e62393a6e04e287685bf507bebf9955dce72
FatmeHassan <fatimahasan.ishtari@gmail.com>
2022-11-05 14:16:26 +0200

    no message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div>
         <div>Home page</div>
-        <Link to="product" className='text-blue-500 '>product page</Link>
+        <Link to="product" className='text-blue-500'>product page</Link>
         <Link to="cart" className='text-blue-500'>cart page</Link>
         <Link to="category" className='text-blue-500'>category page</Link>
         
