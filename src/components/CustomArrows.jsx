b7dcbae9d22b5219c4bec4133d4de4c6e3bc4076
omar zrayka <nickobabychoppa@gmail.com>
2022-12-01 13:34:26 +0200

    no message

--- src/components/CustomArrows.jsx
+++ src/components/CustomArrows.jsx
@@ -12,9 +12,9 @@ export default function CustomArrows({
     <div
       style={{ ...style, padding: "2px 2px" }}
       onClick={onClick}
-      className={`absolute z-10 ${
+      className={`absolute z-10 top-1/2 -mt-9  ${
         direction === "r" ? "right-0" : "left-0"
-      }  border-dblack1 border w-9 h-9  top-1/2  cursor-pointer bg-dgreyTransp1 mx-4 flex items-center justify-center`}
+      }  border-dblack1 border w-9 h-9   cursor-pointer bg-dgreyTransp1 mx-4 flex items-center justify-center`}
     >
  
 
@@ -34,9 +34,9 @@ export default function CustomArrows({
     <div
       style={{ ...style, padding: "2px 5px" }}
       onClick={onClick}
-      className={` absolute z-20 ${
+      className={` absolute z-20 top-1/2 -mt-9 ${
         direction === "r" ? "right-0" : "left-0"
-      }  rounded-full w-14 h-14  top-1/2  cursor-pointer -mx-12`}
+      }  rounded-full w-14 h-14   cursor-pointer -mx-12`}
     >
       {/* <img src={direction === 'r' ? right : left} alt="" className='w-16 h-16 transform rounded-full hover:scale-125 ease-in-out' /> */}
       {direction === "r" ? (
