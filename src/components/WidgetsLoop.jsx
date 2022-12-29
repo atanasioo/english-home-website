c1f806d0080d0c857de066721ced7f22ef39d2f7
Eomarfzraika <omar.1.zraika@gmail.com>
2022-12-29 15:34:58 +0200

    .

--- src/components/WidgetsLoop.jsx
+++ src/components/WidgetsLoop.jsx
@@ -149,7 +149,7 @@ function WidgetsLoop({ widget, showCartmenu }) {
         <div className="">
           {window.innerWidth > 650 ? (
             <Slider
-              {...productSetting}
+              {... widget.items[0].product_id ?  productSetting : carousal}
               beforeChange={handleBeforeChange}
               afterChange={handleAfterChange}
               className="carousel place-items-center"
