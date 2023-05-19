b862313fcf8b9cf9fea6be0978c4c9d45808831c
FatmeHassan <fatimahasan.ishtari@gmail.com>
2023-05-19 11:03:05 +0300

    /

--- src/components/ProductZoomModal.jsx
+++ src/components/ProductZoomModal.jsx
@@ -116,10 +116,10 @@ function ProductZoomModal(props) {
           />
           <div
             id="popup_modal"
-            className="flex flex-col justify-center h-full"
+            className="flex flex-col justify-center h-900 lg:h-full"
           >
             <div className=" flex flex-col h-full lg:h-unset justify-between lg:flex-row lg:mx-8 py-2 md:py-0  lg:bg-white">
-              <div className="product-big-img lg:mr-3 w-full lg:w-2/3 flex flex-col justify-center items-center">
+              <div className="product-big-img lg:mr-3 w-full lg:w-2/3 flex flex-col h-full justify-center items-center">
                 {width > 840 ? (
                   <PrismaZoom
                     minZoom={1}
@@ -162,7 +162,7 @@ function ProductZoomModal(props) {
                   </Slider>
                 )}
               </div>
-              <div className="product-info ml-4 w-1/3">
+              <div className="product-info ml-4 lg:w-1/3">
                 <div className="flex flex-col text-dborderblack2 text-left place-content-center">
                   <div className="product-title hidden lg:block font-mono md:font-mono font-semibold  text-dborderblack2 text-d17 md:text-d20 w-8/12">
                     {productData.name}
