93a911bd07b80fdd1bc2a76a49917ec678b76c2f
FatmeHassan <fatimahasan.ishtari@gmail.com>
2023-08-04 15:16:53 +0300

    fixed token multiple request

--- src/axios.js
+++ src/axios.js
@@ -6,19 +6,19 @@ async function getToken() {
   let requestBody = {
     client_id: "shopping_oauth_client",
     client_secret: "shopping_oauth_secret",
-    source_id: 1
+    source_id: 1,
   };
   let requestHeader = {
-    Authorization: "Basic dGVzdGNsaWVudDp0ZXN0cGFzcw=="
+    Authorization: "Basic dGVzdGNsaWVudDp0ZXN0cGFzcw==",
   };
- 
+
   axios
     .post(buildLink("token"), requestBody, {
-      headers: requestHeader
+      headers: requestHeader,
     })
     .then((response) => {
       Cookies.set("api-token", response.data.access_token, { expires: 15 });
-      
+
       excuting = false;
       window.location.reload();
     });
@@ -31,41 +31,97 @@ if (typeof Cookies.get("api-token") === "undefined") {
 }
 
 const _axios = axios.create({
-  headers: { Authorization: "Bearer " + Cookies.get("api-token") }
+  headers: { Authorization: "Bearer " + Cookies.get("api-token") },
 });
-console.log("======================");
-console.log("create");
+
 _axios.interceptors.request.use(
   function (config) {
     return config;
   },
   function (error) {
-    console.log("======================");
-    console.log("reject");
     return Promise.reject(error);
   }
 );
+// _axios.interceptors.response.use(
+//   function (response) {
+
+//     return response;
+//   },
+//   function (error) {
+
+//     if (
+//       typeof error.response !== "undefined" &&
+//       error?.response?.status === 401
+//     ) {
+//       window.location.reload();
+
+//       // getToken();
+//       //     if(!excuting){
+//       // getToken();}
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
+
+let isRefreshing = false;
+let refreshSubscribers = [];
+
+const subscribeTokenRefresh = (callback) => {
+  refreshSubscribers.push(callback);
+};
+
+const onRefreshed = (token) => {
+  refreshSubscribers.forEach((callback) => callback(token));
+  refreshSubscribers = [];
+};
+
 _axios.interceptors.response.use(
-  function (response) {
-    console.log("======================");
-    console.log("use");
+  (response) => {
     return response;
   },
-  function (error) {
-    console.log("======================");
-    console.log("retry");
+  async (error) => {
     if (
-      typeof error.response !== "undefined" &&
-      error?.response?.status === 401
+      error?.response?.status === 401 &&
+      !error.config._retry &&
+      !isRefreshing
     ) {
-      window.location.reload();
+      error.config._retry = true;
+      isRefreshing = true;
 
-      // getToken();
-      //     if(!excuting){
-      // getToken();}
+      let requestBody = {
+        client_id: "shopping_oauth_client",
+        client_secret: "shopping_oauth_secret",
+        source_id: 1,
+      };
+      let requestHeader = {
+        Authorization: "Basic dGVzdGNsaWVudDp0ZXN0cGFzcw==",
+      };
+
+      try {
+        // Request a new token
+        const response = await axios.post(buildLink("token"), requestBody, {
+          headers: requestHeader,
+        });
+        const newToken = response.data.access_token;
+
+        // Update the token in cookie
+        Cookies.set("api-token", response.data.access_token, { expires: 15 });
+
+        // Trigger the onRefreshed callback to notify other requests
+        onRefreshed(newToken);
+        // Refresh the page
+        window.location.reload();
+        // Retry the original request with the new token
+        error.config.headers.Authorization = `Bearer ${newToken}`;
+        return _axios(error.config);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
     }
-    console.log("======================");
-    console.log("reject");
+
     return Promise.reject(error);
   }
 );
